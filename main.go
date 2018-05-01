1c6851dce829d4d71587d9b23188a80d0109bcc8
gabidev <bkahn95@gmail.com>
2018-05-02 00:59:27 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+// templ은 하나의 템플릿을 나타냄
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP가 HTTP 요청을 처리한다
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+}
+
+func main() {
+	// 루트
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	// 웹서버 시작
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
